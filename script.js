05956848557f199b24425106bad04107cad2b601
Rayyan Siddiqui <rayyansiddiqui772@gmail.com>
2024-10-26 00:02:24 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+function displayVal(userVal) {
+    var inputVal = document.getElementById("calValue");
+    inputVal.value += userVal;
+}
+
+function resetCal() {
+    var inputVal = document.getElementById("calValue");
+    inputVal.value += "";
+}
+
+
+function delCal() {
+    var inputVal = document.getElementById("calValue");
+    inputVal.value += inputVal.value.slice(0, -1);
+}
+
+
+function displayResult() {
+    var inputVal = document.getElementById("calValue");
+    try{
+        var result = eval(inputVal.value);
+        inputVal.value = result;
+    } catch {
+        inputVal.value = "Error";
+    }
+    
+}
+
